3c935bb27f66cfabc64a8bcb15d6433611e5a198
dandobai <dandobai@gmail.com>
2026-01-21 22:50:02 +0100

    feat(backend): enable multipart support and initialize image handling config

--- backend/src/main/kotlin/com/dandobai/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/dandobai/backend/BackendApplication.kt
@@ -2,10 +2,23 @@ package com.dandobai.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.github.cdimascio.dotenv.dotenv
 
 @SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
+	val dotenv = dotenv {
+		ignoreIfMalformed = true
+		ignoreIfMissing = true // Fontos: CI/CD környezetben (pl. Docker) nem lesz .env fájl
+	}
+
+	// Ez betölti a .env tartalmát a rendszer környezeti változói közé
+	dotenv.entries().forEach { System.setProperty(it.key, it.value) }
+
+	dotenv.entries().forEach { entry ->
+		System.setProperty(entry.key, entry.value)
+	}
+
 	runApplication<BackendApplication>(*args)
 }
